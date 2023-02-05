0e677b81f249ce7d8ae5fc1d89a54bdd6c7dc0fc
lazoryk <lazorkinv@gmail.com>
2023-02-05 12:03:13 +0200

    add common functionality

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, render_template
+
+
+application = Flask(__name__, template_folder="templates")
+
 
 @application.route("/healthz")
 def index():
-    return "ZAPRETI MNE NOSIT` STONE ISLAND"
+    return render_template('heathz.html')
+
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port='8080')
+    application.run(host='0.0.0.0', port='8080', debug=True)
